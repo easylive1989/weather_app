e80685d6bbb6ec05ff22de76f6cd94bdc2901279
Paul <easylive1989@gmail.com>
2022-07-17 17:32:14 +0800

    remove unused code and add weather list screen

---
+++ lib/views/weather_list_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WeatherListScreen extends StatelessWidget {
+  const WeatherListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
