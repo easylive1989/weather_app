6126a5a0c1b4ae8e1be20aef566abd8ea09f1d0f
Paul <easylive1989@gmail.com>
2022-07-17 17:57:07 +0800

    add title

--- lib/views/weather_list_screen.dart
+++ lib/views/weather_list_screen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class WeatherListScreen extends StatelessWidget {
   const WeatherListScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(
+          "World Weather",
+          style: TextStyle(color: Colors.blue, fontSize: 30, height: 2.5),
+        ),
+      ),
+    );
   }
 }
